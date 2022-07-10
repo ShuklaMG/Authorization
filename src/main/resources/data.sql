f838a75314dc29efcca5b69a95b4ae3030524f95
Shukla <shuklamg99@gmail.com>
2022-07-10 23:35:50 +0530

    first commit

---
+++ src/main/resources/data.sql
@@ -0,0 +1,8 @@
+drop table if exists userrecord;
+create table userrecord(user_id int primary key ,user_name varchar(30),user_password varchar(20));
+
+
+INSERT INTO userrecord (user_id,user_name,user_password) VALUES (101,'jay','jay');
+INSERT INTO userrecord (user_id,user_name,user_password) VALUES (102,'sanjana','sanjana');
+INSERT INTO userrecord (user_id,user_name,user_password) VALUES (103,'harshita','harshita');
+INSERT INTO userrecord (user_id,user_name,user_password) VALUES (104,'anand','anand');
